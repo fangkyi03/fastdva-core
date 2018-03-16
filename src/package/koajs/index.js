47d899e8b7d5c23b8fedffdadd3d8f5b6b2b0eae
fangkyi02 <fangkyi02@live.cn>
2018-03-16 11:26:58 +0800

    初始化提交

---
+++ src/package/koajs/index.js
@@ -0,0 +1,5 @@
+import createStore from './src/createStore'
+
+module.exports = {
+    createStore
+}
---
+++ src/package/koajs/src/createStore.js
@@ -0,0 +1,17 @@
+export default function createStore(option) {
+    const app = {
+        _store:{},
+        _model:{},
+        model,
+        dispatch,
+    }
+    return app
+
+    function model(m) {
+        
+    }
+
+    function dispatch(params) {
+        
+    }
+}
